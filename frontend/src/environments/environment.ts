984f68351b07ebcd9d26c3b6d5afee7d6db287c0
Nadia Mezini <nadia.mezini@italiacoding.com>
2025-11-17 15:11:39 +0100

    add backend to BE routes on FE

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: "https://libratrack-production.up.railway.app/api",
+  apiUrl: "https://libratrack-production.up.railway.app/backend/api",
 }
