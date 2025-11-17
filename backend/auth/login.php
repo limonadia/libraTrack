70010ce5f46ccd6585e6652f53ffd89de927c5e1
Nadia Mezini <nadia.mezini@italiacoding.com>
2025-11-17 17:27:21 +0100

    Remove autoload/php

--- backend/auth/login.php
+++ backend/auth/login.php
@@ -15,7 +15,6 @@ header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-require_once __DIR__ . '/../../vendor/autoload.php';
 use \Firebase\JWT\JWT;
 
 include_once '../config/database.php';
